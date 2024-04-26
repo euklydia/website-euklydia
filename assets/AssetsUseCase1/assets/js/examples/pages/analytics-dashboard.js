ab106bea08be6ea407dd0bf74f2d206aa71e9026
Tejeddine MtaAllah <tejeddine.mtaalah@gmail.com>
2024-04-26 15:47:25 +0100

    Beta

--- assets/AssetsUseCase1/assets/js/examples/pages/analytics-dashboard.js
+++ assets/AssetsUseCase1/assets/js/examples/pages/analytics-dashboard.js
@@ -245,34 +245,27 @@ $(function () {
         if ($('#analytics-tab-1').length) {
             var options = {
                 series: [{
-                    name: '',
-                    data: [  40, 19, 30, 22,12, 11, 7, 5, 18, 12, 11, 40, 21, 26, 12, 15, 22, 11, 14, 17, 10,22, 11, 14, 17, 10,11, 14, 78, 10,22, 11, 14, 17, 10, 10,11, 14, 10,22, 11, 14, 17, 10,14, 10,22, 11, 14, 17, 10
-                    ]
+                    name: 'Reach',
+                    data: [10961, 13566, 10848, 11144, 10790, 11187, 8422, 4891, 6734, 2347, 228, 887, 315, 2267, 1432, 1998, 485, 278, 173, 11234, 17202, 24201, 18273, 3216, 26820, 18513, 25250, 21496, 16516]
                 }],
                 chart: {
                     height: 490,
                     type: 'area',
                     offsetX: -20,
-                     width: '102%',
+                    width: '102%',
                     fontFamily: chartFontStyle,
                     toolbar: {
-                        show: false,
-                        tickAmount: 3, // Will result in 4 horizontal grid lines including the bottom one
-
-                    },
-                   
-                    
+                        show: false
+                    }
                 },
                 dataLabels: {
                     enabled: false
                 },
-                
                 stroke: {
                     width: 1,
                     curve: 'smooth'
                 },
                 xaxis: {
-                    
                     type: 'datetime',
                     categories: [
                         "2024-02-01", "2024-02-02", "2024-02-03", "2024-02-04", "2024-02-05",
@@ -280,29 +273,21 @@ $(function () {
                         "2024-02-11", "2024-02-12", "2024-02-13", "2024-02-14", "2024-02-15",
                         "2024-02-16", "2024-02-17", "2024-02-18", "2024-02-19", "2024-02-20",
                         "2024-02-21", "2024-02-22", "2024-02-23", "2024-02-24", "2024-02-25",
-                        "2024-02-26", "2024-02-27", "2024-02-28", "2024-02-29", "2024-03-01",
-                        "2024-03-02", "2024-03-03", "2024-03-04", "2024-03-05", "2024-03-06",
-                        "2024-03-07", "2024-03-08", "2024-03-09", "2024-03-10", "2024-03-11",
-                        "2024-03-12", "2024-03-13", "2024-03-14", "2024-03-15", "2024-03-16",
-                        "2024-03-17", "2024-03-18", "2024-03-19", "2024-03-20", "2024-03-21",
-                        "2024-03-26"
+                        "2024-02-26", "2024-02-27", "2024-02-28", "2024-02-29"
                     ]
-                                                    },
-
-                                        
+                },
                 colors: [colors.success],
-
                 fill: {
                     type: 'gradient',
                     gradient: {
                         opacityFrom: .6,
-                        opacityTo: 0,
+                        opacityTo: 0
                     }
-                },
+                }
             };
-
+    
             var chart = new ApexCharts(document.querySelector("#analytics-tab-1"), options);
-
+    
             chart.render();
         }
     }
@@ -311,9 +296,8 @@ $(function () {
         if ($('#analytics-tab-2').length) {
             var options = {
                 series: [{
-                    name: 'Conversations',
-                    data: [  0, 5, 10, 13,12, 11, 7, 5, 12, 11, 21, 26, 12, 15, 22, 11, 14, 17, 10,22, 11, 14, 17, 10,11, 14, 10,22, 11, 14, 17, 10, 10,11, 14, 10,22, 11, 14, 17, 10,14, 10,22, 11, 14, 17, 10
-                    ]
+                    name: 'Reach',
+                    data: [1969, 2041, 1565, 1380, 937, 1044, 660, 357, 314, 108, 7, 89, 33, 159, 55, 252, 53, 15, 14, 1010, 2270, 2360, 1260, 4, 18, 7, 319, 615, 736]
                 }],
                 chart: {
                     height: 490,
@@ -322,19 +306,16 @@ $(function () {
                     width: '102%',
                     fontFamily: chartFontStyle,
                     toolbar: {
-                        show: false,
-                    },
+                        show: false
+                    }
                 },
-                
+                dataLabels: {
+                    enabled: false
+                },
                 stroke: {
                     width: 1,
                     curve: 'smooth'
                 },
-
-                dataLabels: {
-                    enabled: false
-                },
-
                 xaxis: {
                     type: 'datetime',
                     categories: [
@@ -343,12 +324,7 @@ $(function () {
                         "2024-02-11", "2024-02-12", "2024-02-13", "2024-02-14", "2024-02-15",
                         "2024-02-16", "2024-02-17", "2024-02-18", "2024-02-19", "2024-02-20",
                         "2024-02-21", "2024-02-22", "2024-02-23", "2024-02-24", "2024-02-25",
-                        "2024-02-26", "2024-02-27", "2024-02-28", "2024-02-29", "2024-03-01",
-                        "2024-03-02", "2024-03-03", "2024-03-04", "2024-03-05", "2024-03-06",
-                        "2024-03-07", "2024-03-08", "2024-03-09", "2024-03-10", "2024-03-11",
-                        "2024-03-12", "2024-03-13", "2024-03-14", "2024-03-15", "2024-03-16",
-                        "2024-03-17", "2024-03-18", "2024-03-19", "2024-03-20", "2024-03-21",
-                        "2024-03-26"
+                        "2024-02-26", "2024-02-27", "2024-02-28", "2024-02-29"
                     ]
                 },
                 colors: [colors.secondary],
@@ -356,13 +332,13 @@ $(function () {
                     type: 'gradient',
                     gradient: {
                         opacityFrom: .6,
-                        opacityTo: 0,
+                        opacityTo: 0
                     }
-                },
+                }
             };
-
+    
             var chart = new ApexCharts(document.querySelector("#analytics-tab-2"), options);
-
+    
             chart.render();
         }
     }
@@ -371,8 +347,8 @@ $(function () {
         if ($('#analytics-tab-3').length) {
             var options = {
                 series: [{
-                    name: 'Bounce Rate',
-                    data: [20.5, 30.6, 25.6, 22.6, 25.1, 15.5, 18.0]
+                    name: 'Facebook Visits',
+                    data: [1009, 1074, 729, 529, 785, 799, 1264, 1079, 812, 349, 350, 499, 367, 684, 713, 598, 359, 402, 426, 893, 1160, 1412, 1102, 659, 774, 673, 1493, 1126, 1067]
                 }],
                 chart: {
                     height: 490,
@@ -380,10 +356,14 @@ $(function () {
                     offsetX: -20,
                     offsetY: 20,
                     width: '102%',
-                    fontFamily: chartFontStyle,
+                    fontFamily: 'sans-serif', // Ensure you define `chartFontStyle` or replace it with a string
                     toolbar: {
                         show: false,
                     }
+                },
+
+                dataLabels: {
+                    enabled: false
                 },
                 stroke: {
                     width: 1,
@@ -392,44 +372,47 @@ $(function () {
                 xaxis: {
                     type: 'datetime',
                     categories: [
-                        "01 feb",
-                        "02 feb",
-                        "03 feb",
-                        "04 feb",
-                        "05 feb",
-                        "06 feb",
-                        "07 feb"
+                        "2024-02-01T00:00:00", "2024-02-02T00:00:00", "2024-02-03T00:00:00",
+                        "2024-02-04T00:00:00", "2024-02-05T00:00:00", "2024-02-06T00:00:00",
+                        "2024-02-07T00:00:00", "2024-02-08T00:00:00", "2024-02-09T00:00:00",
+                        "2024-02-10T00:00:00", "2024-02-11T00:00:00", "2024-02-12T00:00:00",
+                        "2024-02-13T00:00:00", "2024-02-14T00:00:00", "2024-02-15T00:00:00",
+                        "2024-02-16T00:00:00", "2024-02-17T00:00:00", "2024-02-18T00:00:00",
+                        "2024-02-19T00:00:00", "2024-02-20T00:00:00", "2024-02-21T00:00:00",
+                        "2024-02-22T00:00:00", "2024-02-23T00:00:00", "2024-02-24T00:00:00",
+                        "2024-02-25T00:00:00", "2024-02-26T00:00:00", "2024-02-27T00:00:00",
+                        "2024-02-28T00:00:00", "2024-02-29T00:00:00"
                     ]
                 },
-                colors: [colors.info],
+                colors: ['#00A3E0'], // Replace `colors.info` with a specific color if necessary
                 fill: {
                     type: 'gradient',
                     gradient: {
-                        opacityFrom: .6,
+                        opacityFrom: 0.6,
                         opacityTo: 0,
                     }
                 },
                 tooltip: {
                     y: {
                         formatter: function (val) {
-                            return "%" + val
+                            return "%" + val;
                         }
                     }
                 }
             };
-
+    
             var chart = new ApexCharts(document.querySelector("#analytics-tab-3"), options);
-
             chart.render();
         }
     }
+    
 
     function analytics_tab4() {
         if ($('#analytics-tab-4').length) {
             var options = {
                 series: [{
-                    name: 'Session Duration',
-                    data: [25, 30, 25, 32, 25, 30, 18]
+                    name: 'Daily Visits',
+                    data: [36, 28, 31, 24, 26, 15, 27, 12, 15, 6, 4, 9, 1, 9, 8, 24, 28, 7, 10, 15, 42, 36, 21, 4, 5, 3, 8, 26, 9]
                 }],
                 chart: {
                     height: 490,
@@ -437,7 +420,72 @@ $(function () {
                     offsetX: -20,
                     offsetY: 20,
                     width: '102%',
-                    fontFamily: chartFontStyle,
+                    fontFamily: 'sans-serif', // make sure you define `chartFontStyle` or replace it with a string
+                    toolbar: {
+                        show: false,
+                    }
+                },
+
+
+                dataLabels: {
+                    enabled: false
+                },
+                stroke: {
+                    width: 1,
+                    curve: 'smooth'
+                },
+                xaxis: {
+                    type: 'datetime',
+                    categories: [
+                        "2024-02-01T00:00:00", "2024-02-02T00:00:00", "2024-02-03T00:00:00",
+                        "2024-02-04T00:00:00", "2024-02-05T00:00:00", "2024-02-06T00:00:00",
+                        "2024-02-07T00:00:00", "2024-02-08T00:00:00", "2024-02-09T00:00:00",
+                        "2024-02-10T00:00:00", "2024-02-11T00:00:00", "2024-02-12T00:00:00",
+                        "2024-02-13T00:00:00", "2024-02-14T00:00:00", "2024-02-15T00:00:00",
+                        "2024-02-16T00:00:00", "2024-02-17T00:00:00", "2024-02-18T00:00:00",
+                        "2024-02-19T00:00:00", "2024-02-20T00:00:00", "2024-02-21T00:00:00",
+                        "2024-02-22T00:00:00", "2024-02-23T00:00:00", "2024-02-24T00:00:00",
+                        "2024-02-25T00:00:00", "2024-02-26T00:00:00", "2024-02-27T00:00:00",
+                        "2024-02-28T00:00:00", "2024-02-29T00:00:00"
+                    ]
+                },
+                colors: ['#00A3E0'], // Replace `colors.info` with a specific color if necessary
+                fill: {
+                    type: 'gradient',
+                    gradient: {
+                        opacityFrom: 0.6,
+                        opacityTo: 0,
+                    }
+                },
+                tooltip: {
+                    y: {
+                        formatter: function (val) {
+                            return val + " visits";
+                        }
+                    }
+                }
+            };
+    
+            var chart = new ApexCharts(document.querySelector("#analytics-tab-4"), options);
+            chart.render();
+        }
+    }
+    
+
+    function analytics_tab5() {
+        if ($('#analytics-tab-5').length) {
+            var options = {
+                series: [{
+                    name: 'Visitor Numbers',
+                    data: [38, 29, 25, 18, 25, 32, 26, 22, 23, 7, 12, 14, 13, 5, 11, 7, 8, 6, 12, 28, 40, 25, 27, 13, 18, 9, 22, 9, 20]
+                }],
+                chart: {
+                    height: 490,
+                    type: 'area',
+                    offsetX: -20,
+                    offsetY: 20,
+                    width: '102%',
+                    fontFamily: 'sans-serif', // make sure you define `chartFontStyle` or replace it with a string
                     toolbar: {
                         show: false,
                     }
@@ -449,31 +497,108 @@ $(function () {
                 xaxis: {
                     type: 'datetime',
                     categories: [
-                        "01 feb 2020 18:50",
-                        "02 feb 2020 18:50",
-                        "03 feb 2020 18:50",
-                        "04 feb 2020 18:50",
-                        "05 feb 2020 18:50",
-                        "06 feb 2020 18:50",
-                        "07 feb 2020 18:50"
+                        "2024-02-01T00:00:00", "2024-02-02T00:00:00", "2024-02-03T00:00:00",
+                        "2024-02-04T00:00:00", "2024-02-05T00:00:00", "2024-02-06T00:00:00",
+                        "2024-02-07T00:00:00", "2024-02-08T00:00:00", "2024-02-09T00:00:00",
+                        "2024-02-10T00:00:00", "2024-02-11T00:00:00", "2024-02-12T00:00:00",
+                        "2024-02-13T00:00:00", "2024-02-14T00:00:00", "2024-02-15T00:00:00",
+                        "2024-02-16T00:00:00", "2024-02-17T00:00:00", "2024-02-18T00:00:00",
+                        "2024-02-19T00:00:00", "2024-02-20T00:00:00", "2024-02-21T00:00:00",
+                        "2024-02-22T00:00:00", "2024-02-23T00:00:00", "2024-02-24T00:00:00",
+                        "2024-02-25T00:00:00", "2024-02-26T00:00:00", "2024-02-27T00:00:00",
+                        "2024-02-28T00:00:00", "2024-02-29T00:00:00"
                     ]
                 },
-                colors: [colors.warning],
+                colors: [colors.danger], // Replace `colors.info` with a specific color if necessary
                 fill: {
                     type: 'gradient',
                     gradient: {
-                        opacityFrom: .6,
+                        opacityFrom: 0.6,
                         opacityTo: 0,
                     }
                 },
+                dataLabels: {
+                    enabled: false
+                },
+                tooltip: {
+                    y: {
+                        formatter: function (val) {
+                            return val + " visitors";
+                        }
+                    }
+                }
             };
-
-            var chart = new ApexCharts(document.querySelector("#analytics-tab-4"), options);
-
+    
+            var chart = new ApexCharts(document.querySelector("#analytics-tab-5"), options);
             chart.render();
         }
     }
+    
 
+    function analytics_tab6() {
+        if ($('#analytics-tab-6').length) {
+            var options = {
+                series: [{
+                    name: 'User Engagements',
+                    data: [2, 5, 2, 1, 4, 3, 3, 2, 1, 2, 2, 1, 3, 4, 2, 1, 11, 7, 1, 1, 2, 4, 2]
+                }],
+                chart: {
+                    height: 490,
+                    type: 'area',
+                    offsetX: -20,
+                    offsetY: 20,
+                    width: '102%',
+                    fontFamily: 'sans-serif', // ensure `chartFontStyle` is defined or replaced with a string
+                    toolbar: {
+                        show: false,
+                    }
+                },
+                stroke: {
+                    width: 1,
+                    curve: 'smooth'
+                },
+
+                dataLabels: {
+                    enabled: false
+                },
+                
+                xaxis: {
+                    type: 'datetime',
+                    categories: [
+                        "2024-02-01T00:00:00", "2024-02-02T00:00:00", "2024-02-03T00:00:00",
+                        "2024-02-04T00:00:00", "2024-02-05T00:00:00", "2024-02-06T00:00:00",
+                        "2024-02-07T00:00:00", "2024-02-08T00:00:00", "2024-02-09T00:00:00",
+                        "2024-02-10T00:00:00", "2024-02-12T00:00:00", "2024-02-14T00:00:00",
+                        "2024-02-15T00:00:00", "2024-02-16T00:00:00", "2024-02-17T00:00:00",
+                        "2024-02-20T00:00:00", "2024-02-21T00:00:00", "2024-02-22T00:00:00",
+                        "2024-02-23T00:00:00", "2024-02-24T00:00:00", "2024-02-27T00:00:00",
+                        "2024-02-28T00:00:00", "2024-02-29T00:00:00"
+                    ]
+                },
+                colors: ['#00A3E0'], 
+                // replace with your color variable or preferred color
+                fill: {
+                    type: 'gradient',
+                    gradient: {
+                        opacityFrom: 0.6,
+                        opacityTo: 0,
+                    }
+                },
+                tooltip: {
+                    y: {
+                        formatter: function (val) {
+                            return val + " engagements";
+                        }
+                    }
+                }
+            };
+    
+            var chart = new ApexCharts(document.querySelector("#analytics-tab-6"), options);
+            chart.render();
+        }
+    }
+    
+   
     function session_by_channel() {
         var options = {
             series: [742, 1742, 442, 1242],
@@ -507,6 +632,10 @@ $(function () {
     analytics_tab3();
 
     analytics_tab4();
+
+    analytics_tab5();
+
+    analytics_tab6();
 
     device_session_chart();
 
