5ac6396d50f6ef4eb1eb43381e4943ed338a7f76
Tejeddine MtaAllah <tejeddine.mtaalah@gmail.com>
2024-05-27 10:34:07 +0100

    responsive1

--- common.js
+++ common.js
@@ -145,3 +145,90 @@ document.addEventListener('DOMContentLoaded', () => {
     loadHeader();
     loadFooter();
 });
+
+function loadFooter() {
+    const footerHTML = `
+    <div id="footer-placeholder" class="footer-scope">
+    <footer class="footer-section style-2" style="background-color: #00372C; width: 100%; padding:0px 0;">
+        <div class="upper-footer" style="width: 100%; padding: 20px 15px;">
+            <div class="container" style="max-width: 100%;">
+                <div class="row">
+                    <div class="col col-lg-3 col-md-6 col-sm-12 col-12">
+                        <div class="widget about-widget">
+                            <div >
+                            <img src="assets/html-demo/images/logo blanc.png" alt="logo" style="width: 210px; height: 55px;">
+                            </div>
+                            <p style="color: white;">Your AI marketing agency that leads the digital revolution worldwide by merging artificial intelligence with human creativity</p>
+                        </div>
+                    </div>
+                    <div class="col col-xl-3 col-lg-2 col-md-6 col-sm-12 col-12">
+                        <div class="widget link-widget">
+                            <div class="widget-title">
+                            <h3 style="color: white; font-family: 'Schibsted Grotesk', sans-serif;">Pages</h3>
+                            </div>
+                            <ul>
+                                <li><a href="./" style="color: white;">Home</a></li>
+                                <li><a href="./service" style="color: white;">Services</a></li>
+                                <li><a href="./about" style="color: white;">About Us</a></li>
+                                <li><a href="./iaimpact" style="color: white;">AI Impact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col col-lg-3 col-md-6 col-sm-12 col-12">
+                        <div class="widget contact-widget">
+                            <div class="widget-title">
+                            <h3 style="color: white; font-family: 'Schibsted Grotesk', sans-serif;">Address</h3>
+                            </div>
+                            <div class="contact-ft">
+                                <ul style="color: white;">
+                                    <li style="color: white;"><i class="icon-Group-7042" style="color: white;"></i>+33 6 19 09 14 48</li>
+                                    <li style="color: white;"><i class="icon-Group-7043" style="color: white;"></i>contact@euklydia.com</li>
+                                    <li style="color: white;"><i class="icon-Group-7044" style="color: white;"></i>Tunis, Tunisia <br>
+                                    Paris, France</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col col-xl-3 col-lg-3 col-md-6 col-sm-12 col-12">
+                        <div class="widget subscribe">
+                            <div class="widget-title">
+                            <h3 style="color: white; font-family: 'Schibsted Grotesk', sans-serif;"> Get Free Estimate</h3>
+                            </div>
+                            <form action="#">
+                                <div class="form-field">
+                                    <input type="email" placeholder="Your e-mail address" id="email" required="">
+                                    <button type="submit"><i class="icon-flyer"></i></button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="lower-footer" style="width: 100%; padding: 10px 15px;">
+            <div class="container" style="max-width: 100%;">
+                <div class="row align-items-center">
+                    <div class="col col-lg-6 col-12">
+                        <div class="copy-right">
+                            <p class="copyright" style="color: aliceblue;">© 2024 <a href="./" style="color: #00E37D;">Euklydia</a>, All Rights Reserved.</p>
+                        </div>
+                    </div>
+                    <div class="col col-lg-6 col-12">
+                        <ul class="lower-footer-link">
+                            <li><a href="./contact" style="color: aliceblue;">Terms &amp; Conditions</a></li>
+                            <li><a href="./contact" style="color: aliceblue;">Privacy Policy</a></li>
+                            <li><a href="./contact" style="color: aliceblue;">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</div>
+    `;
+    document.getElementById('footer-placeholder').innerHTML = footerHTML;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadFooter();
+});
